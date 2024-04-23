cdc24ad4fb19746e81b9da541198622b59cb8142
yegor <astery227@gmail.com>
2024-04-23 19:34:35 +0300

    A: Create a separate validator for AddressDto

---
+++ src/Api/FluentValidation/AddressDtoValidator.cs
@@ -0,0 +1,16 @@
+using Api.DTOs;
+using FluentValidation;
+
+namespace Api.FluentValidation;
+
+public class AddressDtoValidator : AbstractValidator<AddressDto>
+{
+    public AddressDtoValidator()
+    {
+        RuleFor(x => x).NotNull();
+        RuleFor(x => x.Street).NotEmpty().Length(0, 100);
+        RuleFor(x => x.City).NotEmpty().Length(0, 40);
+        RuleFor(x => x.State).NotEmpty().Length(0, 2);
+        RuleFor(x => x.ZipCode).NotEmpty().Length(0, 5);
+    }
+}
--- src/Api/FluentValidation/RegisterRequestValidator.cs
+++ src/Api/FluentValidation/RegisterRequestValidator.cs
@@ -9,11 +9,7 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
     {
         RuleFor(x => x.Name).NotEmpty().Length(0, 200);
         RuleFor(x => x.Email).NotEmpty().Length(0, 150).EmailAddress();
-        
-        RuleFor(x => x.Address).NotNull();
-        RuleFor(x => x.Address.Street).NotEmpty().Length(0, 100).When(x => x.Address != null);
-        RuleFor(x => x.Address.City).NotEmpty().Length(0, 40).When(x => x.Address != null);
-        RuleFor(x => x.Address.State).NotEmpty().Length(0, 2).When(x => x.Address != null);
-        RuleFor(x => x.Address.ZipCode).NotEmpty().Length(0, 5).When(x => x.Address != null);
+
+        RuleFor(x => x.Address).SetValidator(new AddressDtoValidator());
     }
 }
