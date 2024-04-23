99c158a259d3b240b3257dcc12f34e2faf5598d9
yegor <astery227@gmail.com>
2024-04-23 19:34:35 +0300

    A: Create a separate validator for AddressDto

---
+++ src/Api/FluentValidation/AddressDtoValidator.cs
@@ -0,0 +1,15 @@
+using Api.DTOs;
+using FluentValidation;
+
+namespace Api.FluentValidation;
+
+public class AddressDtoValidator : AbstractValidator<AddressDto>
+{
+    public AddressDtoValidator()
+    {
+        RuleFor(x => x.Street).NotEmpty().Length(0, 100);
+        RuleFor(x => x.City).NotEmpty().Length(0, 40);
+        RuleFor(x => x.State).NotEmpty().Length(0, 2);
+        RuleFor(x => x.ZipCode).NotEmpty().Length(0, 5);
+    }
+}
