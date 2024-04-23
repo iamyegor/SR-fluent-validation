12b9fc697c30368ad0defaf90073cd6f32e576a3
yegor <astery227@gmail.com>
2024-04-23 20:46:27 +0300

    Q: Validate that at least one contact is provided

--- src/Api/FluentValidation/RegisterRequestValidator.cs
+++ src/Api/FluentValidation/RegisterRequestValidator.cs
@@ -1,4 +1,3 @@
-using System.Text.RegularExpressions;
 using Api.DTOs;
 using FluentValidation;
 
@@ -9,13 +8,15 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
     public RegisterRequestValidator()
     {
         RuleFor(x => x.Name).NotEmpty().Length(0, 200);
-        RuleFor(x => x.Email).NotEmpty().Length(0, 150).EmailAddress();
         RuleFor(x => x.Addresses).SetValidator(new AddressesValidator());
         
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .Length(0, 150)
+            .EmailAddress();
+
         RuleFor(x => x.Phone)
             .NotEmpty()
-            .Must(x => Regex.IsMatch(x, "^[2-9][0-9]{9}$"))
-            .When(x => x.Phone != null, ApplyConditionTo.CurrentValidator)
-            .WithMessage("The phone number is incorrect");
+            .Matches("^[2-9][0-9]{9}$");
     }
 }
