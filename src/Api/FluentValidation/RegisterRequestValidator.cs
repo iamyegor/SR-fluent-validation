f82c1e05588935fa54c914c41679c888d8441faa
yegor <astery227@gmail.com>
2024-04-23 20:46:48 +0300

    A: Validate that at least one contact is provided

--- src/Api/FluentValidation/RegisterRequestValidator.cs
+++ src/Api/FluentValidation/RegisterRequestValidator.cs
@@ -9,14 +9,25 @@ public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
     {
         RuleFor(x => x.Name).NotEmpty().Length(0, 200);
         RuleFor(x => x.Addresses).SetValidator(new AddressesValidator());
-        
+
+        When(x => x.Email == null, () =>
+        {
+            RuleFor(x => x.Phone).NotEmpty();
+        });
+        When(x => x.Phone == null, () =>
+        {
+            RuleFor(x => x.Email).NotEmpty();
+        });
+
         RuleFor(x => x.Email)
             .NotEmpty()
             .Length(0, 150)
-            .EmailAddress();
+            .EmailAddress()
+            .When(x => x.Email != null, ApplyConditionTo.CurrentValidator);
 
         RuleFor(x => x.Phone)
             .NotEmpty()
-            .Matches("^[2-9][0-9]{9}$");
+            .Matches("^[2-9][0-9]{9}$")
+            .When(x => x.Phone != null);
     }
 }
