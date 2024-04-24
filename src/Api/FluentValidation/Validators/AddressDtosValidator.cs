7fd49baeb759f95d36c6ddfe40205078bfd8dddb
yegor <astery227@gmail.com>
2024-04-24 14:26:55 +0300

    A: Custom validation rule

--- src/Api/FluentValidation/Validators/AddressDtosValidator.cs
+++ src/Api/FluentValidation/Validators/AddressDtosValidator.cs
@@ -1,4 +1,5 @@
 using Api.DTOs;
+using Api.FluentValidation.CustomRules;
 using FluentValidation;
 
 namespace Api.FluentValidation.Validators;
@@ -8,8 +9,7 @@ public class AddressDtosValidator : AbstractValidator<AddressDto[]>
     public AddressDtosValidator()
     {
         RuleFor(x => x)
-            .Must(x => x?.Length is >= 1 and <= 3)
-            .WithMessage("The number of addresses must be between 1 and 3")
+            .InRange(1, 3)
             .ForEach(x =>
             {
                 x.NotNull().SetValidator(new AddressDtoValidator());
